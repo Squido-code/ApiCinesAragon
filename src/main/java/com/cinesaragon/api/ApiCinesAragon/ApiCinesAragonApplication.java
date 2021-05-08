505085d8fcae1d72689f89d14f724eee72d8b5e6
Squido <squidocode@hotmail.com>
2021-05-08 11:07:40 +0200

    first commit

---
+++ src/main/java/com/cinesaragon/api/ApiCinesAragon/ApiCinesAragonApplication.java
@@ -0,0 +1,13 @@
+package com.cinesaragon.api.ApiCinesAragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCinesAragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCinesAragonApplication.class, args);
+	}
+
+}
